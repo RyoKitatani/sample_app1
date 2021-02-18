6486912e5cf387d4cbd3de165c5a293a939655fa
RyoKitatani <ryo0929m@gmail.com>
2021-02-18 19:26:09 +0000

    new&create設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210218181113) do
+ActiveRecord::Schema.define(version: 20210218182847) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
