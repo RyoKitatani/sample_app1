44bfe24810e628ad6a7186353944a98c10ff9c16
RyoKitatani <ryo0929m@gmail.com>
2021-02-28 02:59:05 +0000

    READMEてきすテキスト変更

--- spec/factories/list.rb
+++ spec/factories/list.rb
@@ -1,6 +1,6 @@
-Factory.define do
+FactoryBot.define do
   factory :list do
-    title { Facker::Lorem.characters(number:10) }
-    body { Facker::Lorem.characters(number:30) }
+    title {Faker::Lorem.characters(number:10)}
+    body {Faker::Lorem.characters(number:30)}
   end
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -3,7 +3,16 @@
 require 'rails_helper'
 
 describe 'モデルのテスト' do
-  it "有効な投稿内容の場合は保存されるか" do
-    expect(FactoryBot.build(:list).to be_valid)
-  end
+    it "有効な投稿内容の場合は保存されるか" do
+     expect(FactoryBot.build(list)).to be_valid
+    end
 end
+
+
+
+
+
+# describe：モデルのテスト'
+# it：有効な投稿内容の場合は保存されるか
+# エクスペクテーション：データの作成
+# マッチャー：有効であるか
